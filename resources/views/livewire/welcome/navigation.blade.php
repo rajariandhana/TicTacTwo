5daabcc66d8067f5f2e6dee8f1359f34b0f582b0
Ralfazza Rajariandhana <rajariandhana@gmail.com>
2024-08-06 20:46:33 +0700

    init

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,15 +1,15 @@
-<nav class="-mx-3 flex flex-col justify-center items-center">
+<nav class="-mx-3 h-full flex flex-col justify-center items-center gap-y-4">
     @auth
         <a
             href="{{ url('/dashboard') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70"
         >
             Dashboard
         </a>
     @else
         <a
             href="{{ route('login') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+            class="rounded-xl w-64 p-3 text-center bg-indigo-500 text-white font-semibold  ring-1 ring-transparent transition "
         >
             Join game
         </a>
@@ -17,7 +17,7 @@
         @if (Route::has('register'))
             <a
                 href="{{ route('register') }}"
-                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                class="rounded-xl w-64 p-3 text-center bg-indigo-500 text-white font-semibold  ring-1 ring-transparent transition "
             >
                 New game
             </a>
